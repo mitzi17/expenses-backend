7ad54d0ee886e388d0a678c9cc49dfa03a335ce2
Mitzi17 <m_estrada17@live.com>
2022-06-15 16:34:57 -0700

    define actions and set strong params

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,39 @@
 class TransactionsController < ApplicationController
+
+    before_action :set_account
+
+    def index
+        @transactions = @account.transactions
+        render json: @transactions
+    end
+
+    def create
+        @transaction = @account.transactions.new(transaction_params)
+        if @account.update_balance(@transaction) != "Balance too low!"
+            @transaction.save
+            render json: @transaction
+        else
+            render json: {error: "Transaction not created!"}
+        end
+    end
+
+    def show
+        @transaction = Transaction.find(params[:id])
+        render json: @transaction
+    end
+    
+    def destroy
+        
+    end
+
+    private
+
+    def set_account
+        @account = Account.find(params[:account_id])
+    end
+
+    def transaction_params
+        params.require(:transaction).permit(:account_id, :amount, :kind, :date, :description)
+    end
+
 end
