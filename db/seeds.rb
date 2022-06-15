11e8790a42fff141225e59c2903d91c78aaee4f5
Mitzi17 <m_estrada17@live.com>
2022-06-15 13:48:40 -0700

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+account = Account.create( name: 'Checking', balance: 100 )
+
+
+transaction = Transaction.create( account_id: 1, amount: 10, kind: 'deposit', date: Date.today, description: 'won $10' )
+
+transaction_two = Transaction.create( account_id: 1, amount: 10, kind: 'withdraw', date: Date.today, description: 'won $10' )
